3b9872ff9c18dda26beca9dd548e962d81d7b093
hyeonJeongByeon <jungjung1218@gmail.com>
2021-08-20 10:27:37 -0400

    Korean version update

--- VoiceCreator/main/models.py
+++ VoiceCreator/main/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import SuspiciousMultipartForm
 from django.db import models
 from django.db.models import aggregates
 
@@ -12,6 +13,8 @@ class Voice(models.Model):
         return self.voice_code
 
 class UserInput(models.Model):
+    test_id = models.CharField(verbose_name="설문참가자 아이디", max_length=20, default = '0')
+    situation = models.CharField(verbose_name="상황 번호", max_length=20, default='1')
     gender = models.CharField(verbose_name="성별", max_length=20)
     age_group = models.CharField(verbose_name="나이 그룹", max_length=20)
     breathiness = models.CharField(verbose_name="breathiness",max_length=20)
@@ -20,6 +23,6 @@ class UserInput(models.Model):
     variation = models.CharField(verbose_name="variation",max_length=20)
     url = models.TextField(verbose_name="URL_list")
     def __str__(self) -> str:
-        return str(self.age_group + self.breathiness + self.smoothness + self.hoarseness + self.variation)
+        return str(self.test_id + self.situation + self.age_group + self.breathiness + self.smoothness + self.hoarseness + self.variation)
     def url_split(self):
         return self.url.split(',')
--- VoiceCreator/main/views.py
+++ VoiceCreator/main/views.py
@@ -24,6 +24,9 @@ def voice_save(request):
         count += 1
     return render(request,"success.html",{"msg":f"{count}개의 Voice를 저장하였습니다"})
 
+def situation_search():
+    situation_list =['당신은 명절을 맞아 멀리 떨어져 지내는 가족/친구와 오랜만에 안부인사와 근황을 전하려고 합니다. 앞서 설명한 플랫폼을 통해 맞춤형 목소리를 제작하여 원하는 목소리로 통화를 할 수 있다면 당신은 어떤 목소리로 소통하고 싶으십니까?','당신은 한 학기동안 실시간 온라인 수업에 학생으로 참여하려고 합니다. 수업은 매번 질의응답, 발표 및 토론이 활발히 진행되고, 교수자(예: 교수, 교사, 강사) 외 수업을 함께 듣는 다른 학생들과는 이미 서로 얼굴을 알고 있다고 가정합시다. 앞서 설명한 플랫폼을 통해 맞춤형 목소리를 제작하여 원하는 목소리로 수업에 참여를 할 수 있다면 당신은 어떤 목소리로 소통하고 싶으십니까?','당신은 다른 유저와 협력하며 즐기는 게임(예: 오버워치, 배틀그라운드)에서 같은 팀 내 다른 멤버들과 소통하려고 합니다. 이 때, 키보드로 입력하고 화면의 글을 읽어야하는 채팅 대신 보이스톡 기능을 활용하여 참여하기로 합니다. 앞서 설명한 플랫폼을 통해 맞춤형 목소리를 제작하여 원하는 목소리로 게임에 참여를 할 수 있다면 당신은 어떤 목소리로 소통하고 싶으십니까?','당신이 배송받은 물품이 파손된 상태로 도착하여 해당 물품 생산 업체 콜센터 상담원에게 문의하려고 합니다. 앞서 설명한 플랫폼을 통해 맞춤형 목소리를 제작하여 원하는 목소리로 민원상담을 할 수 있다면 당신은 어떤 목소리로 소통하고 싶으십니까?','당신은 당신이 잘 아는 분야에 대한 정보전달용 유튜브 콘텐츠(예: 다큐멘터리)를 제작하려고 합니다. 따라서 당신은 영상 클립, 자막, 목소리를 넣어 영상을 구성하려고 하고 이 영상은 유튜브를 사용하는 불특정 다수에게 보여질 수 있습니다. 앞서 설명한 플랫폼을 통해 맞춤형 목소리를 제작하여 원하는 목소리로 영상을 만들 수 있다면 당신은 어떤 목소리로 소통하고 싶으십니까?']
+
 def voice_search(request,gender,voice_code):
     voice_list = Voice.objects.filter(gender=gender,voice_code=voice_code)
     url_list =[]
@@ -37,6 +40,8 @@ def voice_search(request,gender,voice_code):
 
 def save_user_input(request):
     if request.method == "POST":
+        test_id = request.POST.get('test_id')
+        situation = request.POST.get('situation')
         gender = request.POST.get('gender')
         age = request.POST.get('age')
         breathiness = request.POST.get('breathiness')
@@ -48,6 +53,8 @@ def save_user_input(request):
         for url in url_list:
             url_text += (url+',')
         new_input = UserInput()
+        new_input.test_id = test_id
+        new_input.situation = situation
         new_input.gender = gender
         new_input.age_group = age
         new_input.breathiness = breathiness
